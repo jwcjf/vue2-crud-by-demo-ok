db7d2dc344fa268d8a58467790ffc448e8fd9b55
18316541022@qq.com <18316541022@qq.com>
2017-10-13 17:11:39 +0800

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+
+vue2-crud-by-demo   
+
+power by @cjf
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+请点右上角star，谢谢
---
+++ src/views/foot/foot.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+      <!--页脚-->
+    <footer>
+        <strong>Copyright 2015
+            <a href="http://www.etsystem.cn/" target="blank">©深圳布易科技有限公司</a>.
+        </strong>
+        All rights reserved.
+    </footer>
+  </div>
+</template>
+<script>
+  
+</script>
+<style>
+
+footer{
+	height: 50px;
+	width: 100%;
+	background-color: #FFFFFF;
+	line-height: 50px;
+	text-align: center;
+	box-sizing: border-box;
+	border-top: 1px solid #CCCCCC;
+}
+
+ 
+</style>
